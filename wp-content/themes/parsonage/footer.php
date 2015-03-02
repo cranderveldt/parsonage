c4d51fedc8b05467ba1aba71a55b0a82fb93dbce
cranderveldt <djnaphouse@gmail.com>
2015-03-02 18:24:25 -0500

    wordpress template done

--- wp-content/themes/parsonage/footer.php
+++ wp-content/themes/parsonage/footer.php
@@ -1,11 +1,9 @@
-<div class="clear"></div>
-</div>
-<footer id="footer" role="contentinfo">
-<div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
-</div>
-</footer>
-</div>
-<?php wp_footer(); ?>
+    <footer id="footer">
+      <p>&copy; 2015 The Parsonage Soap Company. All rights reserved.</p>
+      <p>The Parsonage&nbsp;&nbsp;|&nbsp;&nbsp;39644 Lovettsville Road, Lovettsville, VA 20180&nbsp;&nbsp;|&nbsp;&nbsp;(540) 822-5957&nbsp;&nbsp;|&nbsp;&nbsp;<a href="mailto:parsonage@parsonagesoap.com">parsonage@parsonagesoap.com</a></p>
+      <p>Proud Member of <a href="http://realhandmadesoap.com/" target="_blank">Real Handmade Soap.com</a>&nbsp;&nbsp;|&nbsp;&nbsp;Site design and production by Cranderveldt.</p>
+      <?php wp_footer(); ?>
+    </footer>
+  </div>
 </body>
 </html>
